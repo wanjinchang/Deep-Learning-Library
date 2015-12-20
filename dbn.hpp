d0c8b5342ff358b08564fa43d2556b8c2aa1167c
Jian Jin <Jian-23@users.noreply.github.com>
2015-12-20 18:51:29 -0500

    Update dbn.hpp

--- dbn.hpp
+++ dbn.hpp
@@ -3,45 +3,33 @@
 
 #include "util.hpp"
 #include "rbm.hpp"
-#include "layer.hpp"
-#include "upDown.hpp"
 using namespace std;
+
 class DBN {
-	size_t numlayer;
+	size_t numLayer;
 	size_t numData;
 	size_t numEpoch;
-	std::vector<RBMlayer> layers;
+	vector<size_t> arch;
 	ActivationType act;
 	LossType loss;
 	actFunction activation;
 	flowOverLayer layerIn;
 	flowOverLayer layerOut;
-	std::vector<double> error;
+	vector<double> error;
+	vector<cv::Mat> generativeWeight;
+	vector<cv::Mat> generativeBias;
+	vector<cv::Mat> recognitionWeight;
+	vector<cv::Mat> recognitionBias;
 
   public:
-  	DBN (LossType l): loss(l) {}
-  	DBN (std::vector<size_t> rbmSize, ActivationType a, LossType l, size_t ne): act(a), loss(l), numEpoch(ne)
-  	{
-  		setLayer(rbmSize);
-  	}
-  	void s_numEpoch(size_t i) {numEpoch = i;}
-  	void addLayer(RBMlayer &l){layers.push_back(l); numlayer++;}
-  	void setLayer(std::vector<size_t> rbmSize);
+  	DBN (){}
+  	DBN (vector<size_t> rbmSize): arch(rbmSize) {}
+  	void setLayer(vector<size_t> rbmSize){arch = rbmSize;}
   	void train(dataInBatch &trainingData, size_t rbmEpoch, LossType l = MSE, ActivationType a = sigmoid_t);
   	void classifier(dataInBatch &trainingData, dataInBatch &trainingLabel, size_t rbmEpoch, LossType l = MSE, ActivationType a = sigmoid_t);
   	void fineTuning(dataInBatch &inputSet, dataInBatch &label, int epoch);
   	classificationError classify(dataInBatch &testingSet, dataInBatch &testinglabel);
 };
-
-void DBN::setLayer(std::vector<size_t> rbmSize){
-	numlayer = 0;
-	layers.clear();
-	for (size_t i = 0; i < rbmSize.size(); i++){
-		RBMlayer temp(rbmSize[i]);
-		temp.s_index(i);
-		addLayer(temp);
-	}
-}
 
 void DBN::train(dataInBatch &trainingData, size_t rbmEpoch, LossType l, ActivationType a){
 	loss = l;
@@ -51,20 +39,24 @@ void DBN::train(dataInBatch &trainingData, size_t rbmEpoch, LossType l, Activati
 	size_t dataDimension = trainingData[0].cols;
 	size_t inputDimension = dataDimension;
 	dataInBatch input = trainingData;
-	std::cout << "Pretrain " << numlayer << " layers." << std::endl;
-	size_t numlayer = numlayer;
+	std::cout << "Pretrain " << numLayer << " layers." << std::endl;
+	size_t numLayer = arch.size();
 	
 	//Pretrain RBMs
-	for (int i = 0; i < numlayer; i++){
+	generativeWeight.resize(numLayer);
+	generativeBias.resize(numLayer);
+	recognitionWeight.resize(numLayer);
+	recognitionBias.resize(numLayer);
+	for (int i = 0; i < numLayer; i++){
 		std::cout << "Pretrain layer " << i << ": binary" << std::endl; 
-		RBM rbm(inputDimension, layers[i].g_size(), i);
+		RBM rbm(inputDimension, arch[i], i);
 		rbm.singleTrainBinary(input, rbmEpoch, act, loss);
-		layers[i].s_weight(rbm.g_weight());
-		layers[i].s_bias(rbm.g_biasPositive());
-		layers[i].s_biasInFlow(rbm.g_biasNegative());
-		layers[i].s_ACT(act);
+		recognitionWeight[i] = rbm.g_weight();
+		recognitionBias[i] = rbm.g_biasPositive();
+		generativeWeight[i] = rbm.g_weight().t();
+		generativeBias[i] = rbm.g_biasNegative();
 		input = rbm.g_emission();
-		inputDimension = layers[i].g_size();
+		inputDimension = arch[i];
 	}
 }
 
@@ -77,19 +69,24 @@ void DBN::classifier(dataInBatch &trainingData, dataInBatch &trainingLabel, size
 	size_t inputDimension = dataDimension;
 	size_t classDimension = trainingLabel[0].cols;
 	dataInBatch input = trainingData;
-	std::cout << "Pretrain " << numlayer + 1 << " layers." << std::endl;
+	size_t numLayer = arch.size();
+	std::cout << "Pretrain " << numLayer << " layers." << std::endl;
 	
 	//Pretrain RBMs
-	for (int i = 0; i < numlayer - 1; i++){
+	generativeWeight.resize(numLayer);
+	generativeBias.resize(numLayer);
+	recognitionWeight.resize(numLayer);
+	recognitionBias.resize(numLayer);
+	for (int i = 0; i < numLayer - 1; i++){
 		std::cout << "Pretrain layer " << i << ": binary" << std::endl; 
-		RBM rbm(inputDimension, layers[i].g_size(), i);
+		RBM rbm(inputDimension, arch[i], i);
 		rbm.singleTrainBinary(input, rbmEpoch, act, loss);
-		layers[i].s_weight(rbm.g_weight());
-		layers[i].s_bias(rbm.g_biasPositive());
-		layers[i].s_biasInFlow(rbm.g_biasNegative());
-		layers[i].s_ACT(act);
+		recognitionWeight[i] = rbm.g_weight();
+		recognitionBias[i] = rbm.g_biasPositive();
+		generativeWeight[i] = rbm.g_weight().t();
+		generativeBias[i] = rbm.g_biasNegative();
 		input = rbm.g_emission();
-		inputDimension = layers[i].g_size();
+		inputDimension = arch[i];
 	}
 
 	dataInBatch jointPattern;
@@ -99,64 +96,131 @@ void DBN::classifier(dataInBatch &trainingData, dataInBatch &trainingLabel, size
 	}
 	inputDimension += classDimension;
 
-	std::cout << "Pretrain layer" << numlayer << " with classifier: binary" << std::endl; 
-	RBM rbm(inputDimension, layers[numlayer - 1].g_size(), numlayer - 1);
+	std::cout << "Pretrain layer" << numLayer << " with classifier: binary" << std::endl; 
+	RBM rbm(inputDimension, arch[numLayer - 1], numLayer - 1);
 	rbm.dropout(std::max(0.1, (double)classDimension / inputDimension));
 	rbm.singleTrainBinary(jointPattern, rbmEpoch, act, loss);
-	layers[numlayer - 1].s_weight(rbm.g_weight());
-	layers[numlayer - 1].s_bias(rbm.g_biasPositive());
-	layers[numlayer - 1].s_biasInFlow(rbm.g_biasNegative());
-	layers[numlayer - 1].s_ACT(act);
-
+	recognitionWeight[numLayer - 1] = rbm.g_weight();
+	recognitionBias[numLayer - 1] = rbm.g_biasPositive();
+	generativeWeight[numLayer - 1] = rbm.g_weight().t();
+	generativeBias[numLayer - 1] = rbm.g_biasNegative();
 	std::cout << "DBN classifier training is finished." << std::endl;
-	std::cout << numlayer << " layers are trained (As a classifier). " << std::endl;
+	std::cout << numLayer << " layers are trained (As a classifier). " << std::endl;
 }
 
-void DBN::fineTuning(dataInBatch &inputSet, dataInBatch &label, int epoch){
-	upDown(layers, inputSet, label, epoch);
+void DBN::fineTuning(dataInBatch &dataSet, dataInBatch &labelSet, int epoch){
+	size_t numLayer = arch.size();
+	size_t numBatch = dataSet.size();
+	size_t batchSize = dataSet[0].rows;
+	int numClass = labelSet[0].cols;
+	size_t dataDim = dataSet[0].cols;
+	for (int i = 0; i < epoch; i++){
+		for (int k = 0; k < numBatch; k++){
+			cv::Mat input = dataSet[k];
+			cv::Mat label = labelSet[k];
+
+			//Bottem-up Pass
+			vector<cv::Mat> recognition;
+			recognition.resize(numLayer + 1);
+			recognition[0] = input;
+			for (size_t j = 0; j < numLayer - 1; j++){
+				recognition[j + 1] = sigmoidACT(recognition[j], recognitionWeight[j], recognitionBias[j]);
+			}
+			cv::Mat jointPattern;
+			cv::hconcat(label, recognition[numLayer - 1], jointPattern);
+			recognition[numLayer] = sigmoidACT(jointPattern, recognitionWeight[numLayer - 1], recognitionBias[numLayer - 1]);
+
+			cv::Rect dataPart = cv::Rect(numClass, 0, recognition[numLayer - 1].cols, batchSize);
+			cv::Rect labelPart = cv::Rect(0, 0, numClass, batchSize);
+			
+			//Gibbs Sampling on the top layer
+			cv::Mat GBbottom = sigmoidACT(recognition[numLayer], generativeWeight[numLayer - 1], generativeBias[numLayer - 1]);
+			cv::Mat GBtop = sigmoidACT(GBbottom, recognitionWeight[numLayer - 1], recognitionBias[numLayer - 1]);
+
+			//Top-down Pass
+			vector<cv::Mat> generation;
+			generation.resize(numLayer + 1);
+			generation[numLayer - 1] = GBbottom(dataPart);
+			for (size_t j = numLayer - 1; j > 0; j--){
+				generation[j - 1] = sigmoidACT(generation[j], generativeWeight[j - 1], generativeBias[j - 1]);
+			}
+
+			//Transmission in each layer
+			vector<cv::Mat> transmissionDown;
+			vector<cv::Mat>	transmissionUp;
+			transmissionUp.resize(numLayer + 1);
+			transmissionDown.resize(numLayer + 1);
+			for (size_t j = 0; j < numLayer - 1; j++){
+				transmissionDown[j] = sigmoidACT(recognition[j + 1], generativeWeight[j], generativeBias[j]);
+			}
+			for (size_t j = 0; j < numLayer - 1; j++){
+				transmissionUp[j + 1] = sigmoidACT(generation[j], recognitionWeight[j], recognitionBias[j]);
+			}
+			double r = 0.01;
+		
+			//Update of generative parameters
+			for (int j = 0; j < numLayer - 1; j++){
+				generativeWeight[j] += r * (recognition[j + 1].t()) * (recognition[j] - transmissionDown[j]) / batchSize;
+				cv::Mat generativeBiasUpdate = recognition[j] - transmissionDown[j];
+				cv::reduce(generativeBiasUpdate, generativeBiasUpdate, 0, CV_REDUCE_AVG, -1);
+				generativeBias[j] += r * generativeBiasUpdate;
+			}
+			
+			//Updata of recognition parameters
+			for (int j = 0; j < numLayer - 1; j++){
+				recognitionWeight[j] += r * generation[j].t() * (generation[j + 1] - transmissionUp[j + 1]) / batchSize;
+				cv::Mat recognitionBiasUpdate = generation[j + 1] - transmissionUp[j + 1];
+				cv::reduce(recognitionBiasUpdate, recognitionBiasUpdate, 0, CV_REDUCE_AVG, -1);
+				recognitionBias[j] += r * recognitionBiasUpdate;
+			}
+			
+			cv::Mat nStart;
+			cv::Mat nEnd;
+			cv::Mat pStart;
+			cv::Mat pEnd;
+			cv::reduce(jointPattern, nStart, 0, CV_REDUCE_AVG, -1);
+			cv::reduce(GBbottom, nEnd, 0, CV_REDUCE_AVG, -1);
+			cv::reduce(recognition[numLayer], pStart, 0, CV_REDUCE_AVG, -1);
+			cv::reduce(GBtop, pEnd, 0, CV_REDUCE_AVG, -1);
+			//Update of top parameters
+			recognitionWeight[numLayer - 1] += r * ((jointPattern.t()) * recognition[numLayer] - (GBbottom.t()) * GBtop) / batchSize;		
+			recognitionBias[numLayer - 1] += r * (pStart - pEnd);	
+			generativeBias[numLayer - 1] += r * (nStart - nEnd);
+			generativeWeight[numLayer - 1] = recognitionWeight[numLayer - 1].t();
+		}
+	}
 }
 
 classificationError DBN::classify(dataInBatch &testingSet, dataInBatch &testinglabel){
 	size_t classDimension = testinglabel[0].cols;
 	size_t numBatch = testingSet.size();
-	if (!numBatch || !testinglabel.size()){
-		throw errorReport("classify cannot load testing data or label set. Check input.");
-	}
-
-	size_t batchSize = testingSet[0].rows;
-	if (!batchSize || !testinglabel[0].rows){
-		throw errorReport("classify cannot load testing data or label set. Check input.");
-	}
-
 	size_t totalError = 0;
+	int batchSize = testingSet[0].rows;
+	int numLayer = arch.size();
 	for (int i = 0; i < numBatch; i++){
 		double batchError = 0;
 		data batchData = testingSet[i];
 		data batchLabel = testinglabel[i];
 		data inFlow = batchData;
-		for (int k = 0; k < numlayer; k++){
-			cv::Mat w = layers[k].g_weight();
-			cv::Mat b = layers[k].g_bias();
-			actFunction layerACT = layers[k].g_ACT();
-			if (k < numlayer - 1){
-				inFlow = layerACT(inFlow, w, b);
+		for (int k = 0; k < numLayer; k++){
+			if (k < numLayer - 1){
+				inFlow = sigmoidACT(inFlow, recognitionWeight[k], recognitionBias[k]);
 			}
 			else {
 				cv::Mat temp = cv::Mat::zeros(batchSize, classDimension, CV_64FC1);
 				cv::hconcat(temp, inFlow, inFlow);
-				inFlow = layerACT(inFlow, w, b);
+				inFlow = sigmoidACT(inFlow, recognitionWeight[k], recognitionBias[k]);
 			}
 		}
-		cv::Mat w = layers[numlayer - 1].g_weight().t();
-		cv::Mat bv = layers[numlayer - 1].g_biasInFlow();
-		actFunction layerACT = layers[numlayer - 1].g_ACT();
-		inFlow = layerACT(inFlow, w, bv);
+		
+		inFlow = sigmoidACT(inFlow, generativeWeight[numLayer - 1], generativeBias[numLayer - 1]);
 		cv::Rect roi = cv::Rect(0, 0, classDimension, batchSize);
 		cv::Mat predictionMat = inFlow(roi);
+		
 		indexLabel predictionDIST = maxIdxInRow(predictionMat);
 		data labelMatrix = oneOfK(predictionDIST, classDimension);
 		batchError = sum(abs(labelMatrix - batchLabel))[0] / 2;
-		totalError += batchError;	
+		totalError += batchError;
 	}
 	cv::Mat m;
 	double errorRate = (double)totalError / (batchSize * numBatch);
